8122c7f472b7b89376c8d92a9340e99a77dc3114
ee.volkova1 <ee.volkova1@tensor.ru>
2020-09-06 00:52:30 +0300

    routing

--- src/hoc/Navigation/Drawer/Drawer.js
+++ src/hoc/Navigation/Drawer/Drawer.js
@@ -1,15 +1,31 @@
 import React, {Component} from "react";
 import classes from './Drawer.module.css';
+import {NavLink} from "react-router-dom";
 import Backdrop from "../../../components/UI/Backdrop/Backdrop";
 
-const links = [1, 2, 3];
+const links = [
+   {to: '/', label: 'Список', exact: true},
+   {to: '/auth', label: 'Авторизация', exact: false},
+   {to: '/quiz-creator', label: 'Создать тест', exact: false}
+];
 
 class Drawer extends Component {
+
+   clickHandler = () => {
+     this.props.onClose();
+   };
+
+
    renderLinks = () => {
       return links.map((link, index) => {
          return (
             <li key={index}>
-               <a href="/">Link {link}</a>
+               <NavLink
+                  to={link.to}
+                  exact={link.exact}
+                  activeClassName={classes.active}
+                  onClick={this.clickHandler}
+               >{link.label}</NavLink>
             </li>
          )
       })
@@ -29,7 +45,7 @@ class Drawer extends Component {
                   {this.renderLinks()}
                </ul>
             </nav>
-            {this.props.isOpen &&  <Backdrop onClick={this.props.onClose}/>}
+            {this.props.isOpen && <Backdrop onClick={this.props.onClose}/>}
          </>
 
       )
